a6cbc20d9d9ab6e72c404d352980575993b41dee
EmilianoJMC <86630896+EmilianoJMC@users.noreply.github.com>
2024-09-08 20:50:03 -0300

    Acrescimo de colunas

--- DB.sql
+++ DB.sql
@@ -1,4 +1,7 @@
-CREATE TABLE filmes_populares (
+
+-- Tabela Criada previamente ao DB
+
+CREATE TABLE filmes_populares_TMDB (
     id INT AUTO_INCREMENT PRIMARY KEY,
     titulo VARCHAR(255),
     data_lancamento DATE,
@@ -6,3 +9,11 @@ CREATE TABLE filmes_populares (
     sinopse TEXT,
     nota FLOAT
 );
+
+
+-- Ajustes a tabela
+
+ALTER TABLE filmes_populares_TMDB 
+ADD COLUMN linguagem_origem VARCHAR(255),
+ADD COLUMN genero VARCHAR(255),
+ADD COLUMN votos INT;
